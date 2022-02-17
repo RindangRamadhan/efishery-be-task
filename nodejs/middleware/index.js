8cd903b86c53e2423727fa095ffda01f7fa6ef03
Rindang Ramadhan <rindangramadhan10@gmail.com>
2022-02-18 01:26:16 +0700

    refactor(middleware): update response error token

--- nodejs/middleware/index.js
+++ nodejs/middleware/index.js
@@ -5,10 +5,20 @@ module.exports = {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
-    if (token == null) return res.sendStatus(401);
+    if (token == null) {
+      return res.status(401).json({
+        "status": "error",
+        "message": "Unauthorized",
+      });
+    }
     
     jwt.verify(token, process.env.SESSION_SECRET, (err, user) => {
-      if (err) return res.sendStatus(403);
+      if (err) {
+        return res.status(401).json({
+          "status": "error",
+          "message": "Forbidden",
+        });
+      }
 
       req.user = user;
 
